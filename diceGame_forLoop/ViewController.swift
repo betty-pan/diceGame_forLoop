764c4a8e6601c8848cabcf32a506b83cc526d27a
Betty Pan <betty19931218@gmail.com>
2021-01-20 09:36:12 +0800

    Initial Commit

---
+++ diceGame_forLoop/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  diceGame_forLoop
+//
+//  Created by Betty Pan on 2021/1/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
